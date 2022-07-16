9a03b04c3b1ff672d753bcd56055dc888421d82d
sumin <tnals545@naver.com>
2022-07-16 23:38:54 +0900

    22/07/16

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-function App() {
-  return <div></div>;
+import axios from "axios";
+import React, { Component } from "react";
+import "./App.scss";
+
+class App extends Component {
+  state = {
+    images: [],
+  };
+
+  getImages = () => {
+    axios
+      .get("https://api.unplash.com/photos/random", {
+        params: {
+          client_id: "Access Key",
+          count: 30,
+        },
+      })
+      .then((res) => {
+        this.setState({
+          images: [
+            ...this.state.images,
+            ...res.data.map((image) => image.urls.small),
+          ],
+        });
+      });
+  };
+
+  render() {
+    return <div className="App">Let's start</div>;
+  }
 }
 
 export default App;
